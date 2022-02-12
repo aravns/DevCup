bf3121f073b08af1908aedf75e3ea42ef82f8c6c
aravns <pythonjavacppcode@gmail.com>
2022-02-12 17:18:22 +0300

    v0.04

--- devfront/main.js
+++ devfront/main.js
@@ -1,12 +1,21 @@
 var div = document.getElementById('div');
-var body = document.body;
+var dadeDiv = document.getElementsByClassName('d-1')[0];
+var childe = document.getElementsByClassName('childeOfDade');
 
 
-div.onclick = (e) => {
-    div.style.transform = `translate(100px, 100px)`
+div.onmousemove = (e) => {
+    div.style.transform = 
+    `translate(${e.layerX}px, ${e.layerY}px)`
     console.log('lala');
 }
 div.onmouseout = () => {
     div.style.transform = `translate(0, 0)`;
 }
+var x = []
+for (let i = 0; i< Object.keys(childe).length; i++) {
+    x.push(dadeDiv.children[i])
+}
 
+x.map((i) => {
+    i.onmousemove()
+})
